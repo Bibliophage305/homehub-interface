44dbd4d1b7b3408ebac4d64a299680eba67b7690
Chris <christopher.hampson58@gmail.com>
2025-01-20 13:12:42 +0000

    It lives!

--- homehub_interface/homehubauth.py
+++ homehub_interface/homehubauth.py
@@ -1,10 +1,8 @@
 import hashlib
-import math
 import os
 import random
 import json
-import requests
-from urllib.parse import quote
+from homehubrequest import HomeHubRequest
 
 
 class AuthenticationException(Exception):
@@ -12,73 +10,14 @@ class AuthenticationException(Exception):
 
 
 class HomeHubAuth:
-    @property
-    def auth_request_cookie(self):
-        return {
-            "req_id": 1,
-            "sess_id": 0,
-            "basic": False,
-            "user": self.user,
-            "dataModel": {
-                "name": "Internal",
-                "nss": [{"name": "gtw", "uri": "http://sagemcom.com/gateway-data"}],
-            },
-            "ha1": self.auth_key[:10] + self.password + self.auth_key[10:],
-            # "ha1": "ca6e4940afd41d8cd98f00b204e9800998ecf8427e830e7a046fd8d92ecec8e4",
-            "nonce": "",
-        }
 
-    @property
-    def auth_request_data(self):
-        return {
-            "request": {
-                "id": 0,
-                "session-id": 0,
-                "priority": True,
-                "actions": [
-                    {
-                        "id": 0,
-                        "method": "logIn",
-                        "parameters": {
-                            "user": self.user,
-                            "persistent": True,
-                            "session-options": {
-                                "nss": [
-                                    {
-                                        "name": "gtw",
-                                        "uri": "http://sagemcom.com/gateway-data",
-                                    }
-                                ],
-                                "language": "ident",
-                                "context-flags": {
-                                    "get-content-name": True,
-                                    "local-time": True,
-                                },
-                                "capability-depth": 2,
-                                "capability-flags": {
-                                    "name": True,
-                                    "default-value": False,
-                                    "restriction": True,
-                                    "description": False,
-                                },
-                                "time-format": "ISO_8601",
-                            },
-                        },
-                    }
-                ],
-                "cnonce": 745670196,
-                "auth-key": "06a19e589dc848a89675748aa2d509b3",
-            }
-        }
-
-    def __init__(
-        self,
-        session
-    ):
-        self.client_nonce = str(math.floor(4294967295 * (random.uniform(0, 1))))
-        self.server_nonce = None
+    def __init__(self, session):
         self.session = session
-        self.authenticate()
+        self.server_nonce = None
+        self.refresh_client_nonce()
+
+    def refresh_client_nonce(self):
+        self.client_nonce = random.randint(0, 1 << 31 - 1)
 
     @property
     def auth_hash(self):
@@ -88,35 +27,51 @@ class HomeHubAuth:
 
     def authenticate(self):
         print("authenticating with", self.user)
-        headers = {
-            "Cookie": "lang=en; session="
-            + quote(json.dumps(self.auth_request_cookie).encode("utf-8"))
-        }
 
-        request = "req=" + quote(
-            json.dumps(self.auth_request_data, sort_keys=True).encode("utf-8")
+        data = self.session.make_request(
+            [
+                {
+                    "method": "logIn",
+                    "parameters": {
+                        "user": self.user,
+                        "persistent": True,
+                        "session-options": {
+                            "nss": [
+                                {
+                                    "name": "gtw",
+                                    "uri": "http://sagemcom.com/gateway-data",
+                                }
+                            ],
+                            "language": "ident",
+                            "context-flags": {
+                                "get-content-name": True,
+                                "local-time": True,
+                            },
+                            "capability-depth": 2,
+                            "capability-flags": {
+                                "name": True,
+                                "default-value": False,
+                                "restriction": True,
+                                "description": False,
+                            },
+                            "time-format": "ISO_8601",
+                        },
+                    },
+                }
+            ]
         )
 
-        response = requests.post(
-            self.session.url, data=request, headers=headers, timeout=self.session.timeout
-        )
-        data = json.loads(response.text)
-        if response.status_code != 200:
-            raise AuthenticationException(
-                "Failed to authenticate. Status code: %s" % response.status_code
-            )
         if not self._is_successful(data):
             raise AuthenticationException(
-                "Failed to authenticate. Error: %s"
-                % data["reply"]["error"]["description"]
+                f"Failed to authenticate. Error: {data['reply']['error']['description']}"
             )
 
         self.server_nonce = data["reply"]["actions"][0]["callbacks"][0]["parameters"][
             "nonce"
         ]
-        self.session.session_id = data["reply"]["actions"][0]["callbacks"][0]["parameters"][
-            "id"
-        ]
+        self.session.session_id = data["reply"]["actions"][0]["callbacks"][0][
+            "parameters"
+        ]["id"]
 
     @property
     def auth_key(self):
@@ -136,12 +91,12 @@ class HomeHubAuth:
 
     @property
     def ha1(self):
-        return self.auth_key[:10] + self.password + self.auth_key[10:]
+        return self.auth_hash[:10] + self.password + self.auth_hash[10:]
 
     @property
     def auth_cookie(self):
         return {
-            "req_id": self.session.next_request_id,
+            "req_id": self.session.next_request_id + 1,
             "sess_id": self.session.session_id,
             "basic": False,
             "user": self.user,
@@ -149,9 +104,10 @@ class HomeHubAuth:
                 "name": "Internal",
                 "nss": [{"name": "gtw", "uri": "http://sagemcom.com/gateway-data"}],
             },
-            "ha1": self.auth_key[:10] + self.password + self.auth_key[10:],
+            "ha1": self.ha1,
             "nonce": self.server_nonce,
         }
+
 
 class HomeHubAdminAuth(HomeHubAuth):
     def __init__(self, *args, **kwargs):
--- homehub_interface/homehubsession.py
+++ homehub_interface/homehubsession.py
@@ -1,7 +1,5 @@
 import os
-import requests
 import json
-from urllib.parse import quote
 
 from homehubauth import HomeHubGuestAuth, HomeHubAdminAuth
 from homehubrequest import HomeHubRequest
@@ -33,118 +31,40 @@ class HomeHubSession:
         return f"http://{self.host}/cgi/json-req"
 
     @property
-    def _authentication(self):
+    def authentication(self):
         if self._admin_authentication is not None:
             return self._admin_authentication
         if self._guest_authentication is not None:
             return self._guest_authentication
         return None
-    
+
     @property
     def next_request_id(self):
         return len(self.requests)
 
     def authenticate_guest(self):
-        """
-        Authenticates the client by creating a new session. The session is automatically renewed so
-        there is no need to authenticate again, unless an explicit AuthenticationException is thrown.
-        """
         self._guest_authentication = HomeHubGuestAuth(self)
-        # self.session_id = self._guest_authentication.session_id
-        self.requests.append(None)
+        self._guest_authentication.authenticate()
 
     def authenticate_admin(self):
+        reset_request = HomeHubRequest(self)
+
+        reset_request.add_action({"method": "resetEvents"})
+
+        reset_request.send()
+
+        self.requests = []
+        self.session_id = 0
+        self._guest_authentication = None
+
         self._admin_authentication = HomeHubAdminAuth(self)
+        self._admin_authentication.authenticate()
 
-    def make_request(self, actions, admin_required=False):
-        if self._authentication is None:
+    def make_request(self, actions):
+        if self.authentication is None:
             self.authenticate_guest()
 
-        headers = {
-            "Cookie": "lang=en; session="
-            + quote(json.dumps(self._authentication.auth_cookie).encode("utf-8"))
-        }
-
-        print("old method cookie")
-        print(self._authentication.auth_cookie)
-        print()
-
-        request_data = self._authentication.request_object
-
-        request_data["request"]["actions"] = actions
-
-        print("old method request data")
-        print(request_data)
-        print()
-
-        request = "req=" + quote(
-            json.dumps(request_data, sort_keys=True).encode("utf-8")
-        )
-
-        response = requests.post(
-            url=self.url, data=request, headers=headers, timeout=self.timeout
-        )
-        if response.status_code == 401:
-            raise AuthenticationException("Session expired")
-        elif response.status_code != 200:
-            raise ResponseException("Failed, got a %s" % response.status_code)
-
-        data = json.loads(response.text)
-        if not self._is_successful(data):
-            if self._is_invalid_user_session(data):
-                self.authenticate_guest()
-            raise ResponseException(
-                "Failed. Reason: %s" % data["reply"]["error"]["description"]
-            )
-
-        # We don't let the request id grow exponentially
-        if self._authentication.request_id > 100000:
-            self.authenticate_guest()
-            self._authentication.request_id = 0
-
-        return data
-
-    def get_hub_light_control(self):
-        actions = [
-            {
-                "id": 0,
-                "method": "getValue",
-                "xpath": "Device/Managers/HubLightControl/LedEnable",
-                "options": {"capability-flags": {"interface": True}},
-            },
-            {
-                "id": 1,
-                "method": "getValue",
-                "xpath": "Device/Managers/HubLightControl/Brightness",
-                "options": {"capability-flags": {"interface": True}},
-            },
-            {
-                "id": 2,
-                "method": "getValue",
-                "xpath": "Device/Managers/HubLightControl/Schedule/Enable",
-                "options": {"capability-flags": {"interface": True}},
-            },
-            {
-                "id": 3,
-                "method": "getValue",
-                "xpath": "Device/Managers/HubLightControl/Schedule/TurnLightOn",
-                "options": {"capability-flags": {"interface": True}},
-            },
-            {
-                "id": 4,
-                "method": "getValue",
-                "xpath": "Device/Managers/HubLightControl/Schedule/TurnLightOff",
-                "options": {"capability-flags": {"interface": True}},
-            },
-        ]
-
-        request = HomeHubRequest(
-            len(self.requests),
-            self.session_id,
-            self.timeout,
-            self.url,
-            self._guest_authentication,
-        )
+        request = HomeHubRequest(self)
 
         for action in actions:
             request.add_action(action)
@@ -154,6 +74,39 @@ class HomeHubSession:
         self.requests.append(request)
 
         return json.loads(request.response.text)
+
+    def get_hub_light_control(self):
+        actions = [
+            {
+                "method": "getValue",
+                "xpath": "Device/Managers/HubLightControl/LedEnable",
+                "options": {"capability-flags": {"interface": True}},
+            },
+            {
+                "method": "getValue",
+                "xpath": "Device/Managers/HubLightControl/Brightness",
+                "options": {"capability-flags": {"interface": True}},
+            },
+            {
+                "method": "getValue",
+                "xpath": "Device/Managers/HubLightControl/Schedule/Enable",
+                "options": {"capability-flags": {"interface": True}},
+            },
+            {
+                "method": "getValue",
+                "xpath": "Device/Managers/HubLightControl/Schedule/TurnLightOn",
+                "options": {"capability-flags": {"interface": True}},
+            },
+            {
+                "method": "getValue",
+                "xpath": "Device/Managers/HubLightControl/Schedule/TurnLightOff",
+                "options": {"capability-flags": {"interface": True}},
+            },
+        ]
+
+        data = self.make_request(actions)
+
+        return data
 
     def get_devices(self) -> list:
         """
@@ -166,7 +119,6 @@ class HomeHubSession:
 
         actions = [
             {
-                "id": 0,
                 "method": "getValue",
                 "xpath": "Device/Hosts/Hosts",
                 "options": {"capability-flags": {"interface": True}},
@@ -175,7 +127,7 @@ class HomeHubSession:
 
         data = self.make_request(actions)
 
-        return self._parse_homehub_response(data)
+        return data
 
     @staticmethod
     def _is_successful(data):
